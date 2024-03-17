386c109879e029765c5a6d63cfcb1fb8499c6b78
finn-moon <121737168+finn-moon@users.noreply.github.com>
2024-03-17 20:47:18 +0000

    Added new file with Dad

---
+++ finn_python.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+var=12
+if var == 0 :
+    print("Var is not 0")
+else :
+    print("Congrats you are cheese!")
+
+if var == 10 :
+    print("I tried to eat cheese")
+else :
+    print("I am a cheese toot")
